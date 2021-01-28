b0edd6db09cc7424a23351980c7516ad2cb8f01c
Julius Blank <julius.blank@proglove.de>
2021-01-28 05:33:41 +0100

    intermediate try

---
+++ users/julius/default.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, ... }:
+let
+  inherit (builtins) toFile readFile;
+  inherit (lib) fileContents mkForce;
+
+  name = "Julius Blank";
+in
+{
+  imports = [ ../../profiles/develop ./graphical ];
+
+  home-manager.users.julius = {
+    imports = [ ../profiles/git ../profiles/alacritty ../profiles/direnv ];
+  };
+
+  users.users.julius = {
+    uid = 1000;
+    description = name;
+    hashedPassword = fileContents ../../secrets/julius;
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+}
